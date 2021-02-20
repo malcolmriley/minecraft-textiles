6320a332d1fdb856029ed046d5b656f92c852380
Malcolm Riley <malcolmriley@users.noreply.github.com>
2021-02-19 19:30:35 -0800

    Generator classes use base classes from library

--- src/main/java/paragon/minecraft/wilytextiles/generators/ItemModelGenerator.java
+++ src/main/java/paragon/minecraft/wilytextiles/generators/ItemModelGenerator.java
@@ -1,22 +1,12 @@
 package paragon.minecraft.wilytextiles.generators;
 
 import net.minecraft.data.DataGenerator;
-import net.minecraftforge.client.model.generators.ItemModelProvider;
-import net.minecraftforge.client.model.generators.ModelFile;
-import net.minecraftforge.client.model.generators.ModelFile.UncheckedModelFile;
 import net.minecraftforge.common.data.ExistingFileHelper;
-import paragon.minecraft.library.Utilities;
+import paragon.minecraft.library.datageneration.ItemModelHelper;
 import paragon.minecraft.wilytextiles.Textiles;
 import paragon.minecraft.wilytextiles.init.ModItems;
 
-final class ItemModelGenerator extends ItemModelProvider {
-
-	/* Internal Fields */
-	protected static final String TEXTURE_DEFAULT = "layer0";
-	protected static final String ITEM_PREFIX = ITEM_FOLDER + Utilities.Strings.DELIMITER_PATH;
-	protected static final String BLOCK_PREFIX = BLOCK_FOLDER + Utilities.Strings.DELIMITER_PATH;
-
-	protected static final ModelFile GENERATED = new UncheckedModelFile(ITEM_PREFIX + "generated");
+final class ItemModelGenerator extends ItemModelHelper {
 
 	ItemModelGenerator(DataGenerator generator, ExistingFileHelper helper) {
 		super(generator, Textiles.MOD_ID, helper);
@@ -38,16 +28,6 @@ final class ItemModelGenerator extends ItemModelProvider {
 		for (String iterated : simpleItems) {
 			this.simpleItem(iterated);
 		}
-	}
-
-	/* Internal Methods */
-
-	protected void blockItem(String name) {
-		this.getBuilder(name).parent(this.getExistingFile(this.modLoc(BLOCK_PREFIX + name)));
-	}
-
-	protected void simpleItem(String name) {
-		this.getBuilder(name).parent(GENERATED).texture(TEXTURE_DEFAULT, this.modLoc(ITEM_PREFIX + name));
 	}
 
 }
--- src/main/java/paragon/minecraft/wilytextiles/generators/RecipeGenerator.java
+++ src/main/java/paragon/minecraft/wilytextiles/generators/RecipeGenerator.java
@@ -5,16 +5,20 @@ import java.util.function.Consumer;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.IFinishedRecipe;
 import net.minecraft.data.RecipeProvider;
+import net.minecraft.data.ShapedRecipeBuilder;
 import net.minecraft.data.ShapelessRecipeBuilder;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.Tags;
+import paragon.minecraft.library.datageneration.RecipeHelper;
 import paragon.minecraft.wilytextiles.Textiles;
 
-final class RecipeGenerator extends RecipeProvider {
-	
-	/* Internal Fields */
-	private static final String CRITERION_PREFIX = "has_";
+final class RecipeGenerator extends RecipeHelper {
+
+	// Existing criterion fields from vanilla
+	private static final String CRITERION_STICKS = "has_stick";
+	private static final String CRITERION_SUGARCANE = "has_reeds";
 
 	public RecipeGenerator(DataGenerator generator) {
 		super(generator);
@@ -28,21 +32,48 @@ final class RecipeGenerator extends RecipeProvider {
 		this.simpleShaplessMulti(registrar, Items.LIGHT_BLUE_DYE, Textiles.ITEMS.FLAX_PALE.get(), 3);
 		this.simpleShaplessMulti(registrar, Items.CYAN_DYE, Textiles.ITEMS.FLAX_VIBRANT.get(), 3);
 		this.simpleShaplessMulti(registrar, Items.PURPLE_DYE, Textiles.ITEMS.FLAX_PURPLE.get(), 3);
+
+		// Wicker
+		ShapedRecipeBuilder.shapedRecipe(Textiles.ITEMS.WICKER.get(), 3)
+			.patternLine("RIR")
+			.patternLine("IRI")
+			.patternLine("RIR")
+			.key('R', Items.SUGAR_CANE)
+			.key('I', Tags.Items.RODS_WOODEN)
+			.addCriterion(CRITERION_STICKS, RecipeProvider.hasItem(Tags.Items.RODS_WOODEN))
+			.addCriterion(CRITERION_SUGARCANE, RecipeProvider.hasItem(Items.SUGAR_CANE))
+			.build(registrar, this.nameFromRecipe(Textiles.ITEMS.WICKER.get(), Items.SUGAR_CANE));
+
+		// Chain Mesh
+		ShapedRecipeBuilder.shapedRecipe(Textiles.ITEMS.CHAIN_MESH.get(), 5)
+			.patternLine("XX")
+			.patternLine("XX")
+			.key('X', Items.CHAIN)
+			.addCriterion(RecipeHelper.criterionName(Items.CHAIN), RecipeProvider.hasItem(Tags.Items.NUGGETS_IRON))
+			.build(registrar, this.nameFromRecipe(Textiles.ITEMS.CHAIN_MESH.get(), Items.IRON_NUGGET));
+
+		// Chainmail Armor
+		ShapedRecipeBuilder.shapedRecipe(Items.CHAINMAIL_HELMET)
+			.patternLine("###")
+			.patternLine("# #")
+			.key('#', Textiles.ITEMS.CHAIN_MESH.get())
+			.addCriterion(RecipeHelper.criterionName(Textiles.ITEMS.CHAIN_MESH), RecipeProvider.hasItem(Textiles.ITEMS.CHAIN_MESH.get()))
+			.build(registrar, this.nameFromRecipe(Items.CHAINMAIL_HELMET, Textiles.ITEMS.CHAIN_MESH.get()));
 	}
-	
+
 	/* Internal Methods */
-	
+
 	protected void simpleShaplessMulti(final Consumer<IFinishedRecipe> registrar, Item output, Item input, int quantity) {
 		ShapelessRecipeBuilder builder = ShapelessRecipeBuilder.shapelessRecipe(output, 1);
 		for (int count = 0; count < quantity; count += 1) {
 			builder.addIngredient(input);
 		}
-		builder.addCriterion(CRITERION_PREFIX, RecipeProvider.hasItem(input));
-		builder.build(registrar, nameFromRecipe(output, input));
+		builder.addCriterion(CRITERION_PREFIX + input.getRegistryName().getPath(), RecipeProvider.hasItem(input));
+		builder.build(registrar, this.nameFromRecipe(output, input));
 	}
-	
-	protected static ResourceLocation nameFromRecipe(Item output, Item input) {
-		return Textiles.createResource(output.getRegistryName().getPath() + "_from_" + input.getRegistryName().getPath());
+
+	protected ResourceLocation nameFromRecipe(Item output, Item input) {
+		return RecipeHelper.nameFromIngredients(Textiles::createResource, output, input);
 	}
 
 }
