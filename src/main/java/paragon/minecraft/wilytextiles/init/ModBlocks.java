c1296ba3aadc30db53f13b04d7726a5662fa8b93
Malcolm Riley <malcolmriley@users.noreply.github.com>
2021-02-23 01:41:52 -0800

    Add unsoaked fibers block to ModBlocks

---
+++ src/main/java/paragon/minecraft/wilytextiles/init/ModBlocks.java
@@ -0,0 +1,29 @@
+package paragon.minecraft.wilytextiles.init;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.ForgeRegistries;
+import paragon.minecraft.library.ContentProvider;
+import paragon.minecraft.wilytextiles.Textiles;
+import paragon.minecraft.wilytextiles.blocks.SoakableBlock;
+
+public class ModBlocks extends ContentProvider<Block> {
+
+	public ModBlocks() {
+		super(ForgeRegistries.BLOCKS, Textiles.MOD_ID);
+	}
+	
+	public final RegistryObject<Block> RAW_FIBERS = this.add(Names.RAW_FIBERS, () -> new SoakableBlock(AbstractBlock.Properties.create(Material.PLANTS).hardnessAndResistance(0.3F)));
+	
+	/* Block Names */
+	
+	public static class Names {
+
+		private Names() {}
+
+		public static final String RAW_FIBERS = "raw_fibers";
+
+	}
+}
