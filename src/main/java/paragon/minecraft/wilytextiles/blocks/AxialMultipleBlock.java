a83dcb67af1e085b29ea2ee1b7609fd330aeb1e5
Malcolm Riley <malcolmriley@users.noreply.github.com>
2021-07-27 16:11:22 -0700

    Implement Bolt of Fabric block

---
+++ src/main/java/paragon/minecraft/wilytextiles/blocks/AxialMultipleBlock.java
@@ -0,0 +1,89 @@
+package paragon.minecraft.wilytextiles.blocks;
+
+import javax.annotation.Nullable;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.EnumProperty;
+import net.minecraft.state.IntegerProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorldReader;
+
+public class AxialMultipleBlock extends Block {
+	
+	/* Blockstate Fields */
+	public static final int MIN_COUNT = 1;
+	public static final int MAX_COUNT = 9;
+	public static final IntegerProperty COUNT = IntegerProperty.create("count", MIN_COUNT, MAX_COUNT);
+	public static final EnumProperty<Direction.Axis> FACING = EnumProperty.create("facing", Direction.Axis.class, Direction.Axis.X, Direction.Axis.Z);
+	
+	public static final VoxelShape SHAPE_1 = VoxelShapes.create(0.0, 0.0, 0.0, 1.0, 1.0 / 3.0, 1.0);
+	public static final VoxelShape SHAPE_2 = VoxelShapes.create(0.0, 0.0, 0.0, 1.0, 2.0 / 3.0, 1.0);
+	public static final VoxelShape SHAPE_3 = VoxelShapes.fullCube();
+
+	public AxialMultipleBlock(Properties properties) {
+		super(properties);
+		this.setDefaultState(this.stateContainer.getBaseState().with(AxialMultipleBlock.FACING, Direction.Axis.X).with(AxialMultipleBlock.COUNT, AxialMultipleBlock.MIN_COUNT));
+	}
+	
+	/* Supertype Override Methods */
+	
+	@Override
+	@Nullable
+	public BlockState getStateForPlacement(BlockItemUseContext context) {
+		BlockState state = context.getWorld().getBlockState(context.getPos());
+		// If the target block is this, add one to the "count" state parameter, else return super
+		return state.isIn(this) ? this.incrementCount(state) : super.getStateForPlacement(context);
+	}
+
+	@Override
+	public VoxelShape getShape(BlockState state, IBlockReader world, BlockPos position, ISelectionContext context) {
+		int count = state.get(SoakableBlock.COUNT);
+		switch (count) {
+			case 6:
+			case 5:
+				return AxialMultipleBlock.SHAPE_3;
+			case 4:
+			case 3:
+				return AxialMultipleBlock.SHAPE_2;
+		}
+		return AxialMultipleBlock.SHAPE_1;
+	}
+
+	@Override
+	public boolean isValidPosition(BlockState state, IWorldReader world, BlockPos position) {
+		final BlockPos below = position.down();
+		return world.getBlockState(below).isSolidSide(world, below, Direction.UP);
+	}
+
+
+	@Override
+	@SuppressWarnings("deprecation") // Return super.isReplaceable as default if not this
+	public boolean isReplaceable(BlockState state, BlockItemUseContext useContext) {
+		return useContext.getItem().getItem().equals(this.asItem()) && (state.get(AxialMultipleBlock.COUNT).intValue() < AxialMultipleBlock.MAX_COUNT) ? true : super.isReplaceable(state, useContext);
+	}
+
+	@Override
+	public void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+		super.fillStateContainer(builder);
+		builder.add(AxialMultipleBlock.FACING, AxialMultipleBlock.COUNT);
+	}
+	
+	/* Internal Methods */
+	
+	protected BlockState incrementCount(BlockState original) {
+		return original.with(AxialMultipleBlock.COUNT, Math.min(AxialMultipleBlock.MAX_COUNT, this.getCountFrom(original) + 1));
+	}
+	
+	protected int getCountFrom(BlockState state) {
+		return state.get(AxialMultipleBlock.COUNT).intValue();
+	}
+
+}
--- src/main/java/paragon/minecraft/wilytextiles/init/ModBlocks.java
+++ src/main/java/paragon/minecraft/wilytextiles/init/ModBlocks.java
@@ -5,10 +5,12 @@ import net.minecraft.block.AbstractBlock.IPositionPredicate;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialColor;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.ForgeRegistries;
 import paragon.minecraft.library.ContentProvider;
 import paragon.minecraft.wilytextiles.Textiles;
+import paragon.minecraft.wilytextiles.blocks.AxialMultipleBlock;
 import paragon.minecraft.wilytextiles.blocks.BlockBasket;
 import paragon.minecraft.wilytextiles.blocks.SoakableBlock;
 import paragon.minecraft.wilytextiles.blocks.TallCrop;
@@ -23,8 +25,15 @@ public class ModBlocks extends ContentProvider<Block> {
 	
 	public final RegistryObject<Block> RAW_FIBERS = this.add(Names.RAW_FIBERS, () -> new SoakableBlock(AbstractBlock.Properties.create(Material.PLANTS).sound(SoundType.VINE).hardnessAndResistance(0.3F).notSolid().setOpaque(ALWAYS_FALSE)));
 	public final RegistryObject<Block> FLAX_CROP = this.add(Names.FLAX_CROP, () -> new TallCrop(AbstractBlock.Properties.create(Material.PLANTS).sound(SoundType.PLANT).hardnessAndResistance(0.6F).notSolid().setOpaque(ALWAYS_FALSE)));
+	public final RegistryObject<Block> BOLT_RED = this.textileBlock(Names.BOLT_RED, MaterialColor.RED);
 	
 	public final RegistryObject<Block> BASKET = this.add(Names.BASKET, () -> new BlockBasket(AbstractBlock.Properties.create(Material.LEAVES).sound(SoundType.BAMBOO).hardnessAndResistance(0.8F)));
+	
+	/* Internal Methods */
+	
+	public RegistryObject<Block> textileBlock(String name, MaterialColor color) {
+		return this.add(name, () -> new AxialMultipleBlock(AbstractBlock.Properties.create(Material.WOOL, color).sound(SoundType.CLOTH).hardnessAndResistance(0.08F).notSolid().setOpaque(ALWAYS_FALSE)));
+	}
 	
 	/* Block Names */
 	
@@ -35,6 +44,7 @@ public class ModBlocks extends ContentProvider<Block> {
 		public static final String RAW_FIBERS = "raw_fibers";
 		public static final String FLAX_CROP = "crop_flax";
 		public static final String BASKET = "basket";
+		public static final String BOLT_RED = "bolt_red";
 
 	}
 }
