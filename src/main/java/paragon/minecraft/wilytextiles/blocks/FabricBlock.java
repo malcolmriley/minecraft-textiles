bf6153b0696567438971a6af81a00e28b0b75c86
Malcolm Riley <malcolmriley@users.noreply.github.com>
2021-08-02 13:06:34 -0700

    FabricBlock is flammable

--- src/main/java/paragon/minecraft/wilytextiles/blocks/FabricBlock.java
+++ src/main/java/paragon/minecraft/wilytextiles/blocks/FabricBlock.java
@@ -6,6 +6,7 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
+import net.minecraft.util.Direction;
 import net.minecraft.util.Direction.Axis;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
@@ -14,20 +15,22 @@ import net.minecraft.world.IBlockReader;
 
 /**
  * Specific implementation of {@link AxialMultipleBlock} for use by Fabric blocks.
- * 
+ *
  * @author Malcolm Riley
  */
 public class FabricBlock extends AxialMultipleBlock {
-	
+
 	/* Constants */
 	private static final float DEFAULT_HARDNESS = 0.08F;
-	
+	private static final int FIRE_ENCOURAGEMENT = 30; // Uses wool block "encouragement" value from FireBlock class (referred to as "spread speed" by current mappings)
+	private static final int FIRE_FLAMMABILITY = 60; // Uses wool block "flammability" value from FireBlock class
+
 	/* Internal Fields */
 	private static final int SHAPE_INSET = 2;
 	public static final VoxelShape SHAPE_X_1 = Block.makeCuboidShape(0 + SHAPE_INSET, 0, 0, 16 - SHAPE_INSET, 16.0 / 3.0, 16);
 	public static final VoxelShape SHAPE_X_2 = Block.makeCuboidShape(0 + SHAPE_INSET, 0, 0, 16 - SHAPE_INSET, 32.0 / 3.0, 16);
 	public static final VoxelShape SHAPE_X_3 = Block.makeCuboidShape(0 + SHAPE_INSET, 0, 0, 16 - SHAPE_INSET, 16, 16);
-	
+
 	public static final VoxelShape SHAPE_Z_1 = Block.makeCuboidShape(0, 0, 0 + SHAPE_INSET, 16, 16.0 / 3.0, 16 - SHAPE_INSET);
 	public static final VoxelShape SHAPE_Z_2 = Block.makeCuboidShape(0, 0, 0 + SHAPE_INSET, 16, 32.0 / 3.0, 16 - SHAPE_INSET);
 	public static final VoxelShape SHAPE_Z_3 = Block.makeCuboidShape(0, 0, 0 + SHAPE_INSET, 16, 16, 16 - SHAPE_INSET);
@@ -35,12 +38,22 @@ public class FabricBlock extends AxialMultipleBlock {
 	public FabricBlock(final Properties properties) {
 		super(properties);
 	}
-	
+
 	public FabricBlock(final MaterialColor color, final IPositionPredicate opacityPredicate) {
 		this(FabricBlock.createPropertiesFrom(color, opacityPredicate));
 	}
-	
+
 	/* Supertype Override Methods */
+
+	@Override
+	public int getFlammability(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
+		return FIRE_FLAMMABILITY;
+	}
+
+	@Override
+	public int getFireSpreadSpeed(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
+		return FIRE_ENCOURAGEMENT;
+	}
 
 	@Override
 	public VoxelShape getShape(BlockState state, IBlockReader world, BlockPos position, ISelectionContext context) {
@@ -58,15 +71,15 @@ public class FabricBlock extends AxialMultipleBlock {
 		}
 		return axis.equals(Axis.X) ? SHAPE_X_1 : SHAPE_Z_1;
 	}
-	
+
 	/* Internal Methods */
-	
+
 	/**
 	 * Method to create default {@link AbstractBlock.Properties} for the {@link FabricBlock} using the provided parameters.
 	 * <p>
 	 * Sets the material to {@link Material#CARPET} (for flammability flag and fluid response), the sound to {@link SoundType#CLOTH},
 	 * sets the hardness and resistance to {@link #DEFAULT_HARDNESS}, as well as setting the block to be non-solid.
-	 * 
+	 *
 	 * @param color - The {@link MaterialColor} to use for the {@link FabricBlock}
 	 * @param opacityPredicate - An opacity checking {@link IPositionPredicate}
 	 * @return A suitable {@link AbstractBlock.Properties} instance to use.
