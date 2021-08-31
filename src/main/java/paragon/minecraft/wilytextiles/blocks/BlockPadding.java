7a4e95888db3b182f6edf8676db1994fe8e26fd9
Malcolm Riley <malcolmriley@users.noreply.github.com>
2021-08-30 21:55:34 -0700

    BlockPadding extends RotatedPillarBlock instead of do-it-myself

--- src/main/java/paragon/minecraft/wilytextiles/blocks/BlockPadding.java
+++ src/main/java/paragon/minecraft/wilytextiles/blocks/BlockPadding.java
@@ -3,9 +3,14 @@ package paragon.minecraft.wilytextiles.blocks;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.RotatedPillarBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialColor;
 import net.minecraft.entity.Entity;
+import net.minecraft.state.EnumProperty;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
@@ -15,16 +20,20 @@ import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 
 /**
- * A block of padding material.
+ * A block of padding material. Aligned to a particular axis.
  * <p>
  * Entities traversing it sink in slightly, and entities falling upon it receive reduced fall damage and a very limited bounce.
  *
  * @author Malcolm Riley
  */
-public class BlockPadding extends Block {
+public class BlockPadding extends RotatedPillarBlock {
+
+	/* BlockState Properties */
+	protected static final EnumProperty<Direction.Axis> AXIS = BlockStateProperties.AXIS;
 
 	/* Internal Fields */
-	protected static final VoxelShape SHAPE_FULL = Block.makeCuboidShape(0, 0, 0, 16, 14, 16);
+	protected static final int SHAPE_OFFSET = 2;
+	protected static final VoxelShape SHAPE_FULL = Block.makeCuboidShape(0, 0, 0, 16, 16 - SHAPE_OFFSET, 16);
 	protected static final float FALL_REDUCTION = 0.8F;
 	protected static final float VELOCITY_REDUCTION = 0.5F;
 	protected static final float NOBOUNCE_VELOCITY_REDUCTION = 0.1F;
@@ -35,8 +44,8 @@ public class BlockPadding extends Block {
 
 	/* Public Methods */
 
-	public static AbstractBlock.Properties createDefaultProperties() {
-		return AbstractBlock.Properties.create(Material.WOOL).sound(SoundType.CLOTH).speedFactor(0.75F).hardnessAndResistance(0.1F);
+	public static AbstractBlock.Properties createPropertiesFrom(MaterialColor color) {
+		return AbstractBlock.Properties.create(Material.CARPET, color).sound(SoundType.CLOTH).speedFactor(0.75F).hardnessAndResistance(0.1F);
 	}
 
 	/* Supertype Override Methods */
@@ -67,6 +76,12 @@ public class BlockPadding extends Block {
 		float horizontalReduction = entity.isSuppressingBounce() ? NOBOUNCE_VELOCITY_REDUCTION : VELOCITY_REDUCTION;
 		float verticalReduction = entity.isSuppressingBounce() ? 0.0F : NOBOUNCE_VELOCITY_REDUCTION;
 		entity.setMotion(motion.mul(motion.x * horizontalReduction, -verticalReduction, motion.z * horizontalReduction));
+	}
+
+	/* Internal Methods */
+	
+	protected static Direction.Axis getAxisFrom(BlockState state) {
+		return state.get(BlockPadding.AXIS);
 	}
 
 }
