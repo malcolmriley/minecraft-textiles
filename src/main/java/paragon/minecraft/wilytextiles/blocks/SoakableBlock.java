41dd8c210e9e2b2a192fd2e6f78cc1fc3d11e593
Malcolm Riley <malcolmriley@users.noreply.github.com>
2022-07-06 16:00:20 -0700

    Fix incorrect waterlog behavior on SoakableBlock

--- src/main/java/paragon/minecraft/wilytextiles/blocks/SoakableBlock.java
+++ src/main/java/paragon/minecraft/wilytextiles/blocks/SoakableBlock.java
@@ -6,6 +6,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SimpleWaterloggedBlock;
@@ -17,7 +18,6 @@ import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.shapes.CollisionContext;
-import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
 /**
@@ -33,9 +33,9 @@ public class SoakableBlock extends Block implements SimpleWaterloggedBlock {
 	public static final int MAX_COUNT = 6;
 	public static final IntegerProperty COUNT = IntegerProperty.create("count", 1, MAX_COUNT);
 	public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
-	public static final VoxelShape SHAPE_1 = Shapes.box(0.0, 0.0, 0.0, 1.0, 1.0 / 3.0, 1.0);
-	public static final VoxelShape SHAPE_2 = Shapes.box(0.0, 0.0, 0.0, 1.0, 2.0 / 3.0, 1.0);
-	public static final VoxelShape SHAPE_3 = Shapes.block();
+	public static final VoxelShape SHAPE_1 = Block.box(1.0, 0.0, 1.0, 15.0, 16.0 / 3.0, 15.0);
+	public static final VoxelShape SHAPE_2 = Block.box(1.0, 0.0, 1.0, 15.0, 16.0 / 3.0, 15.0);
+	public static final VoxelShape SHAPE_3 = Block.box(1.0, 0.0, 1.0, 15.0, 16.0, 15.0);
 
 	public SoakableBlock(Properties properties) {
 		super(properties);
@@ -57,6 +57,15 @@ public class SoakableBlock extends Block implements SimpleWaterloggedBlock {
 	}
 
 	@Override
+	@SuppressWarnings("deprecation") // Return super.updateShape after scheduling water update tick
+	public BlockState updateShape(BlockState state, Direction direction, BlockState neighborState, LevelAccessor world, BlockPos position, BlockPos neighborPosition) {
+		if (SoakableBlock.getWaterlogStateFrom(state)) {
+			world.scheduleTick(position, Fluids.WATER, Fluids.WATER.getTickDelay(world));
+		}
+		return super.updateShape(state, direction, neighborState, world, position, neighborPosition);
+	}
+
+	@Override
 	@SuppressWarnings("deprecation") // Return super.isReplaceable as default if not this
 	public boolean canBeReplaced(BlockState state, BlockPlaceContext useContext) {
 		return useContext.getItemInHand().getItem().equals(this.asItem()) && (state.getValue(SoakableBlock.COUNT).intValue() < SoakableBlock.MAX_COUNT) ? true : super.canBeReplaced(state, useContext);
@@ -69,9 +78,12 @@ public class SoakableBlock extends Block implements SimpleWaterloggedBlock {
 		// If the target block is this, add one to the "count" state parameter
 		if (state.is(this)) {
 			int currentCount = state.getValue(SoakableBlock.COUNT);
-			return state.setValue(SoakableBlock.COUNT, Integer.valueOf(Math.min(SoakableBlock.MAX_COUNT, currentCount + 1)));
+			return state.setValue(SoakableBlock.COUNT, Math.min(SoakableBlock.MAX_COUNT, currentCount + 1));
 		}
 		// Otherwise, update fluid state
+		else if (state.getFluidState().is(Fluids.WATER)) {
+			return this.defaultBlockState().setValue(SoakableBlock.WATERLOGGED, true);
+		}
 		return super.getStateForPlacement(context);
 	}
 
@@ -93,24 +105,24 @@ public class SoakableBlock extends Block implements SimpleWaterloggedBlock {
 	public FluidState getFluidState(BlockState state) {
 		return state.getValue(BlockStateProperties.WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
 	}
-	
+
 	/* Internal Methods */
 
 	/**
 	 * Creates the default {@link BlockState} for this {@link Block}.
-	 * 
+	 *
 	 * @return The default {@link BlockState} for this {@link Block}.
 	 */
 	protected BlockState createDefaultState() {
 		return this.stateDefinition.any()
 			.setValue(SoakableBlock.WATERLOGGED, false)
-			.setValue(SoakableBlock.COUNT, Integer.valueOf(1));
+			.setValue(SoakableBlock.COUNT, 1);
 	}
-	
+
 	protected static boolean getWaterlogStateFrom(BlockState state) {
 		return state.getValue(SoakableBlock.WATERLOGGED).booleanValue();
 	}
-	
+
 	protected static int getCountFrom(BlockState state) {
 		return state.getValue(SoakableBlock.COUNT).intValue();
 	}
